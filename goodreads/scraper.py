f952a963dee28759e9ed4846eec5966d401c71e7
Chankey Pathak <chankey007@gmail.com>
2017-07-31 11:21:05 +0530

    Allow user to get popular or new quotes

--- goodreads/scraper.py
+++ goodreads/scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 class GoodReads(object):
     def __init__(self):
         print "Starting..."
@@ -8,12 +9,17 @@ class GoodReads(object):
     def get_quotes(self, url):
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "lxml")
-        quotes = [quote.text.strip().split("\n")[0] for quote in soup.findAll('div', {'class':'quoteText'})]
+        quotes = [quote.text.strip().split("\n")[0] for quote in soup.findAll('div', {'class': 'quoteText'})]
         return quotes
 
 
 if __name__ == '__main__':
-    url = 'https://www.goodreads.com/quotes'
+    suffix = ''
+    print "Welcome! Choose the option to proceed"
+    choice = input("1. Popular\n2. Recently added")
+    if choice == 2:
+        suffix = 'recently_added'
+    url = 'https://www.goodreads.com/quotes/{}'.format(choice)
     scraper = GoodReads()
     for quote in scraper.get_quotes(url):
         print quote
