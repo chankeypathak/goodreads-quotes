fbc25b8185d8627904c53f5503e706d55fc3d6ed
Chankey Pathak <chankey007@gmail.com>
2017-07-31 11:39:47 +0530

    Format

--- goodreads/scraper.py
+++ goodreads/scraper.py
@@ -16,7 +16,7 @@ class GoodReads(object):
 if __name__ == '__main__':
     suffix = ''
     print "Welcome! Choose the option to proceed"
-    choice = input("1. Popular\n2. Recently added")
+    choice = input("1. Popular\n2. Recently added\n")
     if choice == 2:
         suffix = 'recently_added'
     url = 'https://www.goodreads.com/quotes/{}'.format(suffix)
